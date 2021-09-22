46b83904035304dc63b369cf8a20a1d77d532382
Mueed Dalvi <mueed.dalvi@gmail.com>
2021-09-22 09:17:04 +0530

    Commit 2

---
+++ src/com/codefury/controller/addUser.java
@@ -0,0 +1,50 @@
+package com.codefury.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class addUser
+ */
+@WebServlet("/addUser")
+public class addUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String name=request.getParameter("name");
+		String mail=request.getParameter("mail");
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		long phone=Long.parseLong(request.getParameter("phone"));
+		String dOb=request.getParameter("dOb");
+		String type=request.getParameter("typeOfUser");
+		String address=request.getParameter("address");
+		double wallet=Double.parseDouble(request.getParameter("wallet"));
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
